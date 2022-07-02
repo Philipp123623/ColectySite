32e3223a1a4c4b687d66d0d3b0064ff3482ac498
Philipp Stüve <p.s.stueve@gmail.com>
2022-07-02 20:19:42 +0200

    Design Update🎉

--- portfolio-details.php
+++ portfolio-details.php
@@ -51,7 +51,7 @@
   <header id="header" class="header fixed-top" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
-      <a href="index.php" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+      <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>HeroBiz<span>.</span></h1>
@@ -62,17 +62,17 @@
 
           <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="index.php">Home 1 - index.html</a></li>
+              <li><a href="index.html">Home 1 - index.html</a></li>
               <li><a href="index-2.php">Home 2 - index-2.html</a></li>
               <li><a href="index-3.php">Home 3 - index-3.html</a></li>
               <li><a href="index-4.php">Home 4 - index-4.html</a></li>
             </ul>
           </li>
 
-          <li><a class="nav-link scrollto" href="index.php#about">About</a></li>
-          <li><a class="nav-link scrollto" href="index.php#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="index.php#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="index.php#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
+          <li><a class="nav-link scrollto" href="index.html#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="index.html#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
           <li><a href="blog.php">Blog</a></li>
           <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
@@ -115,12 +115,12 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="index.php#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle d-none"></i>
       </nav><!-- .navbar -->
 
-      <a class="btn-getstarted scrollto" href="index.php#about">Get Started</a>
+      <a class="btn-getstarted scrollto" href="index.html#about">Get Started</a>
 
     </div>
   </header><!-- End Header -->
@@ -134,7 +134,7 @@
         <div class="d-flex justify-content-between align-items-center">
           <h2>Portfolio Details</h2>
           <ol>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.html">Home</a></li>
             <li>Portfolio Details</li>
           </ol>
         </div>
